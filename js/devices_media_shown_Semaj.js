64483333b7a0a45790369bea5be81abab2f5a039
spiderasasen <spiderasasen@gmail.com>
2025-12-15 08:28:18 -0500

    adding the new stuff that semaj sent me. i also added a zooming in feture to the image

--- js/devices_media_shown_Semaj.js
+++ js/devices_media_shown_Semaj.js
@@ -1,9 +1,11 @@
 //getting the image and also the text ids
 const sony_camera = document.getElementById("Sony_Camera");
+const phone = document.getElementById("Phone");
 const all_devices = document.getElementById("all_devices");
 //getting the data values of the items that are in the media item class
 const items = document.querySelectorAll("div[data-value]")
 
+// for the camera
 sony_camera.addEventListener("click", () => {
     console.log(sony_camera, "clicked");
     items.forEach(item => {
@@ -16,6 +18,20 @@ sony_camera.addEventListener("click", () => {
         }
     });
 });
+
+//for the phone
+phone.addEventListener("click", () => {
+    console.log(phone, "clicked");
+    items.forEach(item => {
+        console.log(item.dataset.value);
+        if (item.dataset.value !== "Phone"){
+            item.style.display = "none";
+        }
+        else{
+            item.style.display = "block";
+        }
+    });
+})
 
 all_devices.addEventListener("click", () => {
     console.log(all_devices, "clicked");
